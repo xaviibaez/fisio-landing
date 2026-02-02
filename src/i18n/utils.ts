4771286c2b33361c54b4baf5e98d2e2bd5129d0d
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2026-02-02 23:12:14 +0100

    Refactor: remove duplicated code from blog

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,15 +1,18 @@
 import { ui, defaultLang } from './ui';
 
+export type TranslationKey = keyof (typeof ui)[typeof defaultLang];
+export type TranslationFunction = (key: TranslationKey) => string;
+
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
 
-export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
+export function useTranslations(lang: keyof typeof ui): TranslationFunction {
+  return function t(key: TranslationKey) {
     return ui[lang][key] || ui[defaultLang][key];
-  }
+  };
 }
 
 export function getLocalizedPath(path: string, lang: keyof typeof ui) {
